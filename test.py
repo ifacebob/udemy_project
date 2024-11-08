308d93fa3c997d56c31960c6d2d0cca8d5471717
ifacebob <ugochukwubobby@gmail.com>
2024-11-08 09:20:35 +0100

    Trying to work on responsiveness

---
+++ test.py
@@ -0,0 +1,18 @@
+print("This is file one")
+print(50)
+
+x = 6
+c =7
+d = x + c
+print(d)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+myfunc()
+
+a = "Hello World!"
+print(type(a[0]))
+print(len(a))
+
